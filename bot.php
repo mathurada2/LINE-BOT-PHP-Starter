fd75eff4eb285dc71b9753feaab3a099ec9b5352
mathurada2 <mathurada.me@gmail.com>
2017-04-03 11:42:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,6 +12,10 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') { //  && $event['source']['userId'] == 'kikuanone.aor'
 			// Get text sent
+			$text = "";
+			if($event['message']['text'] == 'กินข้าวยัง') {
+				$text = "ยังไไม่ได้กิน";
+			}
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
