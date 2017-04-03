1be13c5403a70b420d0ff46644c74679c4993ef6
mathurada2 <mathurada.me@gmail.com>
2017-04-03 11:26:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,40 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-// 		if ($event['source']['userId'] == 'kikuanone.aor') {
-// 			// Get text sent
-// 			$text = "stop";
-// 			// Get replyToken
-// 			$replyToken = $event['replyToken'];
-
-// 			// Build message to reply back
-// 			$messages = [
-// 				'type' => 'text',
-// 				'text' => $text
-// 			];
-			
-			
-// 			// Make a POST Request to Messaging API to reply to sender
-// 			$url = 'https://api.line.me/v2/bot/message/reply';
-// 			$data = [
-// 				'replyToken' => $replyToken,
-// 				'messages' => [$messages],
-// 			];
-// 			$post = json_encode($data);    // แปลงค่าที่เราส่งให้ (argument) ให้ออกมาเป็น json
-// 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-// 			$ch = curl_init($url);
-// 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-// 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-// 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// 			$result = curl_exec($ch);
-// 			curl_close($ch);
-			
-// 			echo $result . "\r\n";
-// 		}
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['source']['userId'] == 'kikuanone.aor') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') { //  && $event['source']['userId'] == 'kikuanone.aor'
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
